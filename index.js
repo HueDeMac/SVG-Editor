feec44e1e6480db302d725ca80bba384af43784b
Hue De mac <kuldeepkalyan.KK@gmail.com>
2017-04-03 02:15:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const {app,BrowserWindow} = require('electron')
+const {app,BrowserWindow,ipcMain} = require('electron')
 const $ = require('jquery')
 
 app.on('ready',()=>{
-  mainwindow
+  mainwindow = new BrowserWindow({
+      width : 800,
+      height : 500
+  })
+  mainwindow.loadURL('file://' + __dirname + "/index.html")
+})
+
+ipcMain.on('select',()=>{
+    path  = "file://" + __dirname + fs.cwd('/home/' + os.usrdir() + '/selected.html')
 })
